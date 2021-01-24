ec6839716555497ff6065c7a9a6f43a12cee50c0
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-01-24 17:54:13 +0100

    Update wireguard-tor-server-x86.sh

--- ubuntu/wireguard-tor-server-x86.sh
+++ ubuntu/wireguard-tor-server-x86.sh
@@ -84,8 +84,10 @@ echo "------------------------------------------------------------"
 
 echo "
 deb https://deb.torproject.org/torproject.org focal main
-deb-src https://deb.torproject.org/torproject.org focal main "
->> /etc/apt/sources.list.d/tor.repository
+deb-src https://deb.torproject.org/torproject.org focal main 
+"
+> /etc/apt/sources.list.d/tor.list
+
 curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
