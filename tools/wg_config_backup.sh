3914774cc3453fab833c34db2a9b2df760f3353a
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-08-29 11:02:21 +0200

    Update wg_config_backup.sh

--- tools/wg_config_backup.sh
+++ tools/wg_config_backup.sh
@@ -1,1 +1,17 @@
-placeholder
+#!/bin/bash
+clear
+echo " this script backup yours wireguard server config "
+echo " in case if you need to reinstall your server, but not want to reconfigure your all clients"
+echo "."
+echo "."
+echo "."
+tar -cf  /root/backup_wg_config.tar /etc/wireguard
+tar --append --file=/root/backup_wg_config.tar /root/Wireguard-DNScrypt-VPN-Server.README
+tar --append --file=/root/backup_wg_config.tar /etc/ufw/before.rules
+tar --append --file=/root/backup_wg_config.tar /etc/ufw/before6.rules
+echo "."
+echo "."
+echo "."
+echo " done backupfile is /root/backup_wg_config.tar.gz "
+echo " download / copy / save the file to your needs "
+echo " to restore place the file here /root/backup_wg_config.tar.gz and run ./wg_config_restore.sh "
